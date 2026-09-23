8741405f53f7c1972e41431cf54beef7a067fc32
Nitesh Vashisth <nitesh@Niteshs-MacBook-Air.local>
2026-09-23 20:49:05 +0530

    feat: redirect root URL to /admin/

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.shortcuts import redirect
+
 urlpatterns = [
+    path('', lambda request: redirect('admin/', permanent=False)),
     path('admin/', admin.site.urls),
 ]
 
